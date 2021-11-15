c6e31f58113ca1ae93f1a31816c3ab4b6e912b1d
Alex Antonison <adantonison@gmail.com>
2021-11-15 13:04:18 -0500

    Add method for processing survey results and saving output

--- src/process_survey_results.py
+++ src/process_survey_results.py
@@ -77,10 +77,82 @@ def map_values_for_column(df, target_column, mapped_df):
     return df
 
 
+def process_survey_results(df, survey_column, mapping_df=None):
+    """This method will produce individual dataframes with the specified `survey_column`
+    results processed and ready for anlaysis.
+
+    Args:
+        df (dataframe): The survey dataframe that has the survey results
+        survey_column (str): The survey column of interest
+        mapping_df (dataframe, optional): If provided, this will be used to map values for a specified survey column. Defaults to None.
+
+    Returns:
+        dataframe: A dataframe that includes a single survey column with standardized and mapped results
+    """
+
+    df = df[["role", "years_experience", survey_column]]
+
+    # convert survey column into an array
+    df = df.assign(survey_column=df[survey_column].str.split(","))
+
+    # only keep survey columns that have populated values
+    df = df[pd.notnull(df["survey_column"])]
+
+    # standardize all of the survey results
+    df["survey_column"] = df.apply(lambda row: standardize_survey_list(row["survey_column"]), axis=1)
+
+    df = df.explode("survey_column")
+
+    # reset index and rename column to the survey column
+    # name provided
+    df = df.reset_index(drop=True).drop(columns=survey_column).rename(columns={"survey_column": survey_column})
+
+    # replace mapped values
+    if mapping_df is not None:
+        df = map_values_for_column(df, target_column=survey_column, mapped_df=mapping_df)
+
+    # get rid of null and empty columns
+    df = df[df[survey_column].notnull()]
+    df = df[df[survey_column] != ""]
+
+    return df
+
+
 def main():
     load_env_var("config/env.yml")
+
+    # load survey data
     survey_data_df = load_survey_data()
-    print(survey_data_df.head())
+
+    # load mapping data
+    role_map_df = pd.read_csv(os.environ["role_mapping"])
+    prog_language_map_df = pd.read_csv(os.environ["programming_language_mapping"])
+    prog_package_map_df = pd.read_csv(os.environ["programming_package_mapping"])
+    data_tool_mapping_df = pd.read_csv(os.environ["data_tool_mapping"])
+    cloud_provider_mapping_df = pd.read_csv(os.environ["cloud_provider_mapping"])
+
+    # standardize roles from survey results
+    survey_data_df = map_values_for_column(survey_data_df, "role", role_map_df)
+
+    # process the survey results
+    programming_language_survey_df = process_survey_results(
+        survey_data_df, survey_column="programming_language", mapping_df=prog_language_map_df
+    )
+    programming_package_survey_df = process_survey_results(
+        survey_data_df, survey_column="programming_package", mapping_df=prog_package_map_df
+    )
+    data_tools_survey_df = process_survey_results(
+        survey_data_df, survey_column="data_tool", mapping_df=data_tool_mapping_df
+    )
+    cloud_provider_survey_df = process_survey_results(
+        survey_data_df, survey_column="cloud_provider", mapping_df=cloud_provider_mapping_df
+    )
+
+    # save out to csv
+    programming_language_survey_df.to_csv(os.environ["output_programming_language"], index=False)
+    programming_package_survey_df.to_csv(os.environ["output_programming_package"], index=False)
+    data_tools_survey_df.to_csv(os.environ["output_data_tool"], index=False)
+    cloud_provider_survey_df.to_csv(os.environ["output_cloud_provider"], index=False)
 
 
 if __name__ == "__main__":
