5958a47b392529a9e609b9845151558e554777a9
Alexander Antonison <30709680+alex-antonison@users.noreply.github.com>
2021-11-15 14:39:26 -0600

    Initial survey results completed

--- src/process_survey_results.py
+++ src/process_survey_results.py
@@ -1,5 +1,163 @@
-def add_numbers(x, y):
+import os
+
+import pandas as pd
+import yaml
+
+
+def load_env_var(env_file_path):
+    """Loads all of the environment variables from config/env.yml into
+    the os environment.
+
+    Args:
+        env_file_path (str): The path to the env yml file
+
     """
-    Initial function for getting project structure up and going
+    print("loading environment variables")
+    with open(env_file_path, "r") as stream:
+        try:
+            env_config = yaml.safe_load(stream)
+            for var in env_config["env_var"]:
+                os.environ[var] = env_config["env_var"][var]
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+def load_survey_data():
+    """Reads the survey results from configured location"""
+    columns = [
+        "timestamp",
+        "role",
+        "years_experience",
+        "programming_language",
+        "programming_package",
+        "data_tool",
+        "cloud_provider",
+    ]
+    survey_df = pd.read_csv(os.environ["survey_results"], names=columns, skiprows=1)
+    # do not care about the timestamp column
+    survey_df = survey_df.drop(columns="timestamp")
+
+    return survey_df
+
+
+def standardize_survey_list(row):
+    """This function takes in a list for a given row of a survey result and will
+    lower-case it and strip it of all white space
+
+    Args:
+        row (list): A list of survey results
+
+    Returns:
+        list: standardized survey results
     """
-    return x + y
+    # lowers all strings and strips whitespace
+    clean_row = [x.lower().strip() for x in row]
+    return list(set(clean_row))
+
+
+def map_values_for_column(df, target_column, mapped_df):
+    """This method will loop through each entry in a mapping dataframe
+    and replace the original values with the mapped values.  This is helpful
+    for cleaning up the results for analysis.
+
+    All mapped values can be found in the `mapping/` directory.
+
+    Args:
+        df (Dataframe): The dataframe where the values are being re-mapped
+        target_column (str): The column that will have the values re-mapped
+        mapped_df (Dataframe): The mapping dataframe that has the mappings
+
+    Returns:
+        Dataframe: A dataframe with the newly mapped values
+    """
+
+    for index, row in mapped_df.iterrows():
+        df[target_column] = df[target_column].replace([row[0]], row[1])
+
+    return df
+
+
+def process_survey_results(df, survey_column, mapping_df=None):
+    """This method will produce individual dataframes with the specified `survey_column`
+    results processed and ready for analysis.  Lastly it aggregates the results into a single count
+    column.
+
+    Args:
+        df (dataframe): The survey dataframe that has the survey results
+        survey_column (str): The survey column of interest
+        mapping_df (dataframe, optional): If provided, this will be used to map values for a specified survey column. Defaults to None.
+
+    Returns:
+        dataframe: A dataframe that includes a single survey column with standardized and mapped results
+    """
+
+    df = df[["role", "years_experience", survey_column]]
+
+    # convert survey column into an array
+    df = df.assign(survey_column=df[survey_column].str.split(","))
+
+    # only keep survey columns that have populated values
+    df = df[pd.notnull(df["survey_column"])]
+
+    # standardize all of the survey results
+    df["survey_column"] = df.apply(lambda row: standardize_survey_list(row["survey_column"]), axis=1)
+
+    df = df.explode("survey_column")
+
+    # reset index and rename column to the survey column
+    # name provided
+    df = df.reset_index(drop=True).drop(columns=survey_column).rename(columns={"survey_column": survey_column})
+
+    # replace mapped values
+    if mapping_df is not None:
+        df = map_values_for_column(df, target_column=survey_column, mapped_df=mapping_df)
+
+    # get rid of null and empty columns
+    df = df[df[survey_column].notnull()]
+    df = df[df[survey_column] != ""]
+
+    # aggregate results
+    df = df.groupby(list(df.columns)).size().reset_index(name="count")
+
+    return df
+
+
+def main():
+    load_env_var("config/env.yml")
+
+    # load survey data
+    survey_data_df = load_survey_data()
+
+    # load mapping data
+    role_map_df = pd.read_csv(os.environ["role_mapping"])
+    prog_language_map_df = pd.read_csv(os.environ["programming_language_mapping"])
+    prog_package_map_df = pd.read_csv(os.environ["programming_package_mapping"])
+    data_tool_mapping_df = pd.read_csv(os.environ["data_tool_mapping"])
+    cloud_provider_mapping_df = pd.read_csv(os.environ["cloud_provider_mapping"])
+
+    # standardize roles from survey results
+    survey_data_df = map_values_for_column(survey_data_df, "role", role_map_df)
+
+    # process the survey results
+    programming_language_survey_df = process_survey_results(
+        survey_data_df, survey_column="programming_language", mapping_df=prog_language_map_df
+    )
+    programming_package_survey_df = process_survey_results(
+        survey_data_df, survey_column="programming_package", mapping_df=prog_package_map_df
+    )
+    data_tools_survey_df = process_survey_results(
+        survey_data_df, survey_column="data_tool", mapping_df=data_tool_mapping_df
+    )
+    cloud_provider_survey_df = process_survey_results(
+        survey_data_df, survey_column="cloud_provider", mapping_df=cloud_provider_mapping_df
+    )
+
+    # save out to csv
+    programming_language_survey_df.to_csv(os.environ["output_programming_language"], index=False)
+    programming_package_survey_df.to_csv(os.environ["output_programming_package"], index=False)
+    data_tools_survey_df.to_csv(os.environ["output_data_tool"], index=False)
+    cloud_provider_survey_df.to_csv(os.environ["output_cloud_provider"], index=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_process_survey_results.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+from process_survey_results import map_values_for_column, standardize_survey_list
+
+
+def test_standardize_survey_list():
+    """This test ensures that the standardize_survey_list method correctly cleans up the
+    different iterms returning a clean list for analysis
+    """
+    test_list = ["Pandas", "pandas ", "tidyveRse", " Looker"]
+
+    standardized_list = standardize_survey_list(test_list)
+    # sorting to ensure order for test
+    standardized_list.sort()
+    assert standardized_list == ["looker", "pandas", "tidyverse"]
+
+
+def test_map_values_for_column():
+    """This test shows that the mapping function correctly maps only the target column
+    and correctly maps the desired values"""
+
+    source_df = pd.DataFrame({"col1": ["blue", "red"], "col2": ["val1", "val2"], "col3": ["val3", "val4"]})
+    mapping_df = pd.DataFrame({"orig_col": ["blue", "red"], "mapped_col": ["green", "yellow"]})
+    target_column = "col1"
+
+    final_df = pd.DataFrame({"col1": ["green", "yellow"], "col2": ["val1", "val2"], "col3": ["val3", "val4"]})
+
+    pd.testing.assert_frame_equal(map_values_for_column(source_df, target_column, mapping_df), final_df)
