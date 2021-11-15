58a7bb52d5ffc89443930e54a54363755e267aa1
Alexander Antonison <adantonison@gmail.com>
2021-11-15 11:25:40 -0600

    Remove add numbers and rename test file

--- tests/test_example.py
+++
@@ -1,5 +0,0 @@
-from process_survey_results import add_numbers
-
-
-def test_add():
-    assert add_numbers(1, 2) == 3
---
+++ tests/test_process_survey_results.py
@@ -0,0 +1,13 @@
+def test_create_custom_df():
+    # TODO
+    assert 1 == 1
+
+
+def test_standardize_survey_list():
+    # TODO
+    assert 1 == 1
+
+
+def test_map_values_for_column():
+    # TODO
+    assert 1 == 1
